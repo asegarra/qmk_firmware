9a369687ee4c883a20ecff6c6710b427f9edc399
Angel Segarra <angel.segarra@gmail.com>
2024-01-04 17:07:39 -0500

    move super key to delete key position

--- keyboards/kinesis/keymaps/asegarra/keymap.c
+++ keyboards/kinesis/keymaps/asegarra/keymap.c
@@ -22,13 +22,13 @@
 * |--------+------+------+------+------+------|                           |------+------+------+------+------+--------|
 * | Shift  |   Z  |   X  |   C  |   V  |   B  |                           |   N  |   M  |  ,.  |  .>  |  /?  | Shift  |
 * `--------+------+------+------+------+-------                           `------+------+------+------+------+--------'
-*          | `~   | INS  | Left | Right|                                         | Down | Up   |  [{  |  ]}  |
+*          | `~   | DEL  | Left | Right|                                         | Down | Up   |  [{  |  ]}  |
 *          `---------------------------'                                         `---------------------------'
 *                                        ,-------------.         ,-------------.
-*                                        | Ctrl | Alt  |         | Gui  | Ctrl |
+*                                        | Ctrl | Alt  |         | Alt  | Ctrl |
 *                                 ,------|------|------|         |------+------+------.
 *                                 |      |      | Home |         | PgUp |      |      |
-*                                 | BkSp | Del  |------|         |------|Return| Space|
+*                                 | BkSp | Gui  |------|         |------|Return| Space|
 *                                 |      |      | End  |         | PgDn |      |      |
 *                                 `--------------------'         `--------------------'
 */
@@ -41,10 +41,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                                                                      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
     KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                                                      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                                                                      KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-              KC_GRV,   KC_INS,   KC_LEFT,  KC_RGHT,                                                                                       KC_DOWN,  KC_UP,    KC_LBRC,  KC_RBRC,
-                                                      KC_LCTL,  KC_LALT,                                               KC_RGUI,  KC_RCTL,
+              KC_GRV,   KC_DEL,   KC_LEFT,  KC_RGHT,                                                                                       KC_DOWN,  KC_UP,    KC_LBRC,  KC_RBRC,
+                                                      KC_LCTL,  KC_LALT,                                               KC_RALT,  KC_RCTL,
                                                                 KC_HOME,                                               KC_PGUP,
-                                            KC_BSPC,  KC_DEL,   KC_END,                                                KC_PGDN,  KC_ENTER, KC_SPC
+                                            KC_BSPC,  KC_LGUI,  KC_END,                                                KC_PGDN,  KC_ENTER, KC_SPC
   ),
 
   [KEYPAD] = LAYOUT(
@@ -61,3 +61,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
 };
+
