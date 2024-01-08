469eafa5824433b783e10c49bfa81eb38b3fcdd5
Angel Segarra <angel.segarra@gmail.com>
2024-01-08 00:11:08 -0500

    code formatting

--- keyboards/kinesis/keymaps/asegarra/keymap.c
+++ keyboards/kinesis/keymaps/asegarra/keymap.c
@@ -1,6 +1,6 @@
 // Copyright 2023 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later
- 
+
 #include QMK_KEYBOARD_H
 
 #define QWERTY 0 // Base qwerty
@@ -12,7 +12,7 @@
 * Keymap: Default Layer in Qwerty
 *
 * ,-------------------------------------------------------------------------------------------------------------------.
-* | Caps   |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F8  |  F9  |  F10 |  F12 | PSCR | SLCK | PAUS |  MO0 |  BOOT  |
+* | Caps   |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F8  |  F9  |  F10 |  F12 | PSCR | SLCK | PAUS |  MO1 |  BOOT  |
 * |--------+------+------+------+------+------+---------------------------+------+------+------+------+------+--------|
 * | =+     |  1!  |  2@  |  3#  |  4$  |  5%  |                           |  6^  |  7&  |  8*  |  9(  |  0)  | -_     |
 * |--------+------+------+------+------+------|                           +------+------+------+------+------+--------|
@@ -36,29 +36,27 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [QWERTY] = LAYOUT(
-    KC_CAPS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,         KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SCRL,  KC_PAUS,  MO(1),    QK_BOOT,
-    KC_EQL,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                                                      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
-    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                                                                      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
-    KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                                                      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                                                                      KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-              KC_GRV,   KC_DEL,   KC_LEFT,  KC_RGHT,                                                                                       KC_DOWN,  KC_UP,    KC_LBRC,  KC_RBRC,
-                                                      KC_LCTL,  KC_LALT,                                               KC_RALT,  KC_RCTL,
-                                                                KC_HOME,                                               KC_PGUP,
-                                            KC_BSPC,  KC_LGUI,  KC_END,                                                KC_PGDN,  KC_ENTER, KC_SPC
+    KC_CAPS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,  KC_F8,    KC_F9,  KC_F10,  KC_F11,   KC_F12,   KC_PSCR,  KC_SCRL,  KC_PAUS,  MO(1),    QK_BOOT,
+    KC_EQL,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                                            KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
+    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                                                            KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
+    KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                                            KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                                                            KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
+              KC_GRV,   KC_DEL,   KC_LEFT,  KC_RGHT,                                                                             KC_DOWN,  KC_UP,    KC_LBRC,  KC_RBRC,
+                                                      KC_LCTL,  KC_LALT,                                     KC_RALT,  KC_RCTL,
+                                                                KC_HOME,                                     KC_PGUP,
+                                            KC_BSPC,  KC_LGUI,  KC_END,                                      KC_PGDN,  KC_ENTER, KC_SPC
   ),
 
   [KEYPAD] = LAYOUT(
-
     KC_WAKE,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_TRNS,  KC_NO,
     KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,                                                    KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
     KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,                                                    KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
     KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,                                                    KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
     KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,                                                    KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
               KC_NO,  KC_NO,  KC_NO,  KC_NO,                                                                    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-			                                        KC_NO,  KC_NO,                                    KC_NO,  KC_NO,
+                                              KC_NO,  KC_NO,                                    KC_NO,  KC_NO,
                                                       KC_NO,                                    KC_NO,
                                       KC_NO,  KC_NO,  KC_NO,                                    KC_NO,  KC_NO,  KC_NO
     ),
-
 };
 
