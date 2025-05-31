20fc564e74a62704aab50f3ffda879886e8fa5bf
Angel Segarra <angel.segarra@gmail.com>
2025-05-31 04:24:08 -0400

    update default kb

--- keyboards/kinesis/maso/keymaps/default/keymap.c
+++ keyboards/kinesis/maso/keymaps/default/keymap.c
@@ -2,14 +2,13 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
+#include "quantum.h"
 
 #define QWERTY 0 // Base layer
 #define KEYPAD 1 // Keypad layer
 #define EXTEND 2
 
-#define OS_LSFT OSM(MOD_LSFT)
-#define OS_RSFT OSM(MOD_RSFT)
-#define RCTL_ESC RCTL_T(KC_ESC)
+#define LCESC LCTL_T(KC_ESC)
 
 
 /****************************************************************************************************
@@ -41,27 +40,27 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [QWERTY] = LAYOUT(
-    KC_CAPS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,  KC_F8,    KC_F9,  KC_F10,  KC_F11,   KC_F12,   KC_PSCR,  KC_SCRL,  KC_PAUS,  MO(1),    QK_BOOT,
+    KC_CAPS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,  KC_F8,    KC_F9,  KC_F10,  KC_F11,   KC_F12,   KC_PSCR,  KC_SCRL,  KC_PAUS,  TG(1),    MO(1),
     KC_EQL,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                                            KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                                                            KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
-    CW_TOGG,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                                            KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-    OS_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                                                            KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  OS_RSFT,
+    KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                                            KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                                                            KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,   KC_RSFT,
               KC_GRV,   KC_DEL,   KC_LEFT,  KC_RGHT,                                                                             KC_DOWN,  KC_UP,    KC_LBRC,  KC_RBRC,
-                                                      KC_LCTL,  KC_LALT,                                     KC_RALT,  RCTL_ESC,
+                                                      LCESC,    KC_LALT,                                     KC_RALT,  KC_RCTL,
                                                                 KC_HOME,                                     KC_PGUP,
                                             KC_BSPC,  KC_LGUI,  KC_END,                                      KC_PGDN,  KC_ENTER, KC_SPC
   ),
 
   [KEYPAD] = LAYOUT(
-      KC_NUM,   KC_NUM, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_TRNS,  KC_NO,
-      KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,                                                    KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-      KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,                                                    KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-      KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,                                                    KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-      KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,                                                    KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-                KC_NO,  KC_NO,  KC_NO,  KC_NO,                                                                    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-                                                KC_NO,  KC_NO,                                    KC_NO,  KC_NO,
-                                                        KC_NO,                                    KC_NO,
-                                                KC_NO,  KC_NO,  KC_NO,                                    KC_NO,  KC_NO,    KC_NO
+      QK_RBT,   QK_BOOT, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NUM, KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_TRNS,  KC_NO,
+      KC_NO,    KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,                                                    KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+      KC_NO,    KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,                                                    KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+      KC_NO,    KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,                                                    KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+      KC_NO,    KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,                                                    KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+                KC_NO,   KC_NO,  KC_NO,  KC_NO,                                                                    KC_NO,    KC_NO,    KC_NO,    KC_NO,
+                                                 KC_NO,  KC_NO,                                   KC_NO,  KC_NO,
+                                                         KC_NO,                                   KC_NO,
+                                        KC_NO,   KC_NO,  KC_NO,                                   KC_NO,  KC_NO,   KC_NO
   ),
 
   [EXTEND] = LAYOUT(
@@ -78,21 +77,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    switch (get_highest_layer(state)) {
-    case KEYPAD:
-        writePinLow(LED_COMPOSE_PIN);
-        break;
-    default: //  for any other layers, or the default layer
-        writePinHigh(LED_COMPOSE_PIN);
-        break;
-    }
-  return state;
-}
+  if (IS_LAYER_ON_STATE(state, KEYPAD)) {
+    gpio_write_pin_low(LED_LAYER_PIN);
+  } else {
+    gpio_write_pin_high(LED_LAYER_PIN);
+  }
 
-bool led_update_user(led_t led_state) {
-    writePin(LED_NUM_LOCK_PIN, !led_state.num_lock);
-    writePin(LED_CAPS_LOCK_PIN, !led_state.caps_lock);
-    writePin(LED_SCROLL_LOCK_PIN, !led_state.scroll_lock);
-    // Disable led_update_kb() so that layer indication code doesn't get overridden.
-    return false;
+  return state;
 }
