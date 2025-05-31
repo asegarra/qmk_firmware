15831cd8b567f06d07454297e8135e5255fd6703
Angel Segarra <angel.segarra@gmail.com>
2025-05-31 15:46:10 -0400

    light up leds on startup

--- keyboards/kinesis/maso/maso.c
+++ keyboards/kinesis/maso/maso.c
@@ -2,6 +2,23 @@
 
 void keyboard_pre_init_user(void) {
 
+  uint8_t led_delay_ms = 80;
+
+  /* LED pins setup */
+  gpio_set_pin_output(LED_CAPS_LOCK_PIN);
+  gpio_write_pin_low(LED_CAPS_LOCK_PIN);
+
+  wait_ms(led_delay_ms);
+  gpio_set_pin_output(LED_NUM_LOCK_PIN);   
+  gpio_write_pin_low(LED_NUM_LOCK_PIN);
+  wait_ms(led_delay_ms);
+
+  gpio_set_pin_output(LED_SCROLL_LOCK_PIN);   
+  gpio_write_pin_low(LED_SCROLL_LOCK_PIN);
+  wait_ms(led_delay_ms);
+
+  wait_ms(led_delay_ms);
+
   gpio_set_pin_output(LED_LAYER_PIN);
   gpio_write_pin_low(LED_LAYER_PIN);   
 
