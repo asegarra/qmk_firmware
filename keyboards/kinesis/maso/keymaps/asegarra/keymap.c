657a791c43b7a760747b69c5480defe89c73da9d
Angel Segarra <angel.segarra@gmail.com>
2025-09-13 18:15:31 -0400

    Add colemak

--- keyboards/kinesis/maso/keymaps/asegarra/keymap.c
+++ keyboards/kinesis/maso/keymaps/asegarra/keymap.c
@@ -6,8 +6,9 @@
 
 #define QWERTY 0 // Base layer
 #define DVORAK 1
-#define KEYPAD 2 // Keypad layer
-#define EXTEND 3
+#define COLEMAK 2
+#define KEYPAD 3 // Keypad layer
+#define EXTEND 4
 
 #define LCESC LCTL_T(KC_ESC)
 #define RCESC RCTL_T(KC_ESC)
@@ -42,7 +43,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [QWERTY] = LAYOUT(
-    KC_CAPS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,   KC_F6,    KC_F7,  KC_F8,    KC_F9,  KC_F10,  KC_F11,   KC_F12,   KC_PSCR,  KC_SCRL,  KC_PAUS,  TG(2),    MO(2),
+    KC_CAPS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,   KC_F6,    KC_F7,  KC_F8,    KC_F9,  KC_F10,  KC_F11,   KC_F12,   KC_PSCR,  KC_SCRL,  KC_PAUS,  TG(3),    MO(3),
     KC_EQL,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                                           KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                                                           KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
     KC_GRV,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                                           KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
@@ -54,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [DVORAK] = LAYOUT(
-    QK_BOOT,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,  KC_F6,  KC_F7,  KC_F8,    KC_F9,  KC_F10,  KC_F11,  KC_F12,   KC_PSCR,   KC_SCRL,  KC_PAUS,  TG(2),    MO(2),
+    QK_BOOT,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,  KC_F6,  KC_F7,  KC_F8,    KC_F9,  KC_F10,  KC_F11,  KC_F12,   KC_PSCR,   KC_SCRL,  KC_PAUS,  TG(3),    MO(3),
     KC_EQL,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                                       KC_6,      KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
     KC_TAB,   KC_QUOT,  KC_COMM,  KC_DOT,   KC_P,     KC_Y,                                                       KC_F,      KC_G,     KC_C,     KC_R,     KC_L,     KC_SLSH,
     KC_GRV,   KC_A,     KC_O,     KC_E,     KC_U,     KC_I,                                                       KC_D,      KC_H,     KC_T,     KC_N,     KC_S,     KC_BSLS,
@@ -65,8 +66,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             KC_BSPC,  KC_LGUI,  KC_END,                                 KC_PGDN,  KC_ENTER,  KC_SPC
   ),
 
+  [COLEMAK] = LAYOUT(
+    QK_BOOT,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,  KC_F6,  KC_F7,  KC_F8,    KC_F9,  KC_F10,  KC_F11,  KC_F12,   KC_PSCR,   KC_SCRL,  KC_PAUS,  TG(3),    MO(3),
+    KC_EQL,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                                       KC_6,      KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
+    KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_G,                                                       KC_J,      KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_SLSH,
+    KC_GRV,   KC_A,     KC_R,     KC_S,     KC_T,     KC_D,                                                       KC_H,      KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOTE,
+    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                                                       KC_K,      KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
+              KC_CAPS,  KC_DEL,   KC_LEFT,  KC_RGHT,                                                                         KC_DOWN,  KC_UP,    KC_LBRC,  KC_RBRC,
+                                                      LCESC,    KC_LALT,                                KC_RALT,  RCESC,
+                                                                KC_HOME,                                KC_PGUP,
+                                            KC_BSPC,  KC_LGUI,  KC_END,                                 KC_PGDN,  KC_ENTER,  KC_SPC
+  ),
+
   [KEYPAD] = LAYOUT(
-    QK_RBT,   QK_BOOT, KC_NO,  DF(0),  DF(1),  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NUM, KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_TRNS,  KC_NO,
+    QK_RBT,   QK_BOOT, KC_NO,  DF(0),  DF(1),  DF(2),  KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NUM, KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_TRNS,  KC_NO,
     KC_NO,    KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,                                                    KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
     KC_NO,    KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,                                                    KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
     KC_NO,    KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,                                                    KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
